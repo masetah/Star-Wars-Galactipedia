8adb581c1695d157d1ee8383cfa04bf21cb43953
Maysam Taherian <maysamtaherian@Maysams-MacBook-Pro.local>
2019-06-29 11:57:14 -0500

    Created new div element to house character list

--- app.js
+++ app.js
@@ -41,133 +41,121 @@ $(()=>{
         let name
         name = data.name
         console.log(data)
-        let $li = $('<p>')
-        //$('#characterButton').empty();
-        $li.text(name).appendTo($('#characterButton'))
+        let $li = $('<button>')
+        $li.text(name).appendTo($('#characterList'))
     }
     
     $('.Episode1').on("click", function(e){
-        console.log($('#characterButton').children().length)
+        $('#characterList').empty()
+        console.log($('#characterList').children().length)
         e.preventDefault();
-        $('#characterButton').empty()
-        console.log($('#characterButton').children().length)
+        console.log($('#characterList').children().length)
         $('.infoScreen').empty()
         $('.infoScreen').append(`<h1>${Episode1[0].title}`)
         $('.infoScreen').append(`<p>Released: ${Episode1[0].release_date}/ Directed by: ${Episode1[0].director}`)
         $('.infoScreen').append(`<button>Characters`).attr("id","characterButton")
-        $('.mainScreen').remove()
-        
-    
-        $('.main').prepend('<iframe src="https://giphy.com/embed/3owzVXoDN8iP0w3T68" width="480" height="208" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/starwars-star-wars-the-phantom-menace-3owzVXoDN8iP0w3T68">via GIPHY</a></p>')
+        $('.mainScreen').attr('src',"https://giphy.com/embed/3owzVXoDN8iP0w3T68")
         $('#characterButton').on("click", function(e){
-            //e.preventDefault();
+            $('#characterList').empty()
             for (i = 0; i < Episode1[0].characters.length; i++){
-                //console.log(Episode1[0].characters[i])
-                urlSearch(Episode1[0].characters[i])
-                //console.log(name)        
+                urlSearch(Episode1[0].characters[i])      
         }   
     })})
     
     $('.Episode2').on("click", function(e){
+        $('#characterList').empty()
+        console.log($('#characterList').children().length)
         e.preventDefault();
-        $('#characterButton').empty()
+        console.log($('#characterList').children().length)
         $('.infoScreen').empty()
-        //console.log(Episode2)
         $('.infoScreen').append(`<h1>${Episode2[0].title}`)
         $('.infoScreen').append(`<p>Released: ${Episode2[0].release_date}/ Directed by: ${Episode2[0].director}`)
         $('.infoScreen').append(`<button>Characters`).attr("id","characterButton")
-        $('.mainScreen').text('src',"https://youtu.be/MrDStqPgOEU")
+        $('.mainScreen').attr('src',"https://giphy.com/embed/l1Kst3WE3rg2WRHxu")
         $('#characterButton').on("click", function(e){
-            //e.preventDefault();
+            $('#characterList').empty()
             for (i = 0; i < Episode2[0].characters.length; i++){
-                //console.log(Episode2[0].characters[i])
-                urlSearch(Episode2[0].characters[i])
-                //console.log(name)      
+                urlSearch(Episode2[0].characters[i])      
         }   
     })})
     $('.Episode3').on("click", function(e){
+        $('#characterList').empty()
+        console.log($('#characterList').children().length)
         e.preventDefault();
-        $('#characterButton').empty()
+        console.log($('#characterList').children().length)
         $('.infoScreen').empty()
-        console.log(Episode3)
         $('.infoScreen').append(`<h1>${Episode3[0].title}`)
         $('.infoScreen').append(`<p>Released: ${Episode3[0].release_date}/ Directed by: ${Episode3[0].director}`)
         $('.infoScreen').append(`<button>Characters`).attr("id","characterButton")
-        $('.mainScreen').text('src',"https://youtu.be/MrDStqPgOEU")
+        $('.mainScreen').attr('src',"https://giphy.com/embed/3o84sq21TxDH6PyYms")
         $('#characterButton').on("click", function(e){
-            //e.preventDefault();
+            $('#characterList').empty()
             for (i = 0; i < Episode3[0].characters.length; i++){
-                //console.log(Episode2[0].characters[i])
-                urlSearch(Episode3[0].characters[i])
-                //console.log(name)      
+                urlSearch(Episode3[0].characters[i])      
         }   
     })})
     $('.Episode4').on("click", function(e){
+        $('#characterList').empty()
+        console.log($('#characterList').children().length)
         e.preventDefault();
-        $('#characterButton').empty()
+        console.log($('#characterList').children().length)
         $('.infoScreen').empty()
-        console.log(Episode4)
         $('.infoScreen').append(`<h1>${Episode4[0].title}`)
         $('.infoScreen').append(`<p>Released: ${Episode4[0].release_date}/ Directed by: ${Episode4[0].director}`)
         $('.infoScreen').append(`<button>Characters`).attr("id","characterButton")
-        $('.mainScreen').text('src',"https://youtu.be/MrDStqPgOEU")
+        $('.mainScreen').attr('src',"https://giphy.com/embed/3o84sJXOIrnjvlwnF6")
         $('#characterButton').on("click", function(e){
-            //e.preventDefault();
+            $('#characterList').empty()
             for (i = 0; i < Episode4[0].characters.length; i++){
-                //console.log(Episode2[0].characters[i])
-                urlSearch(Episode4[0].characters[i])
-                //console.log(name)      
+                urlSearch(Episode4[0].characters[i])      
         }   
     })})
     $('.Episode5').on("click", function(e){
+        $('#characterList').empty()
+        console.log($('#characterList').children().length)
         e.preventDefault();
-        $('#characterButton').empty()
+        console.log($('#characterList').children().length)
         $('.infoScreen').empty()
-        console.log(Episode5)
         $('.infoScreen').append(`<h1>${Episode5[0].title}`)
         $('.infoScreen').append(`<p>Released: ${Episode5[0].release_date}/ Directed by: ${Episode5[0].director}`)
         $('.infoScreen').append(`<button>Characters`).attr("id","characterButton")
-        $('.mainScreen').text('src',"https://youtu.be/MrDStqPgOEU")
+        $('.mainScreen').attr('src',"https://giphy.com/embed/xT9DPpf0zTqRASyzTi")
         $('#characterButton').on("click", function(e){
-            //e.preventDefault();
+            $('#characterList').empty()
             for (i = 0; i < Episode5[0].characters.length; i++){
-                //console.log(Episode2[0].characters[i])
-                urlSearch(Episode5[0].characters[i])
-                //console.log(name)      
+                urlSearch(Episode5[0].characters[i])      
         }   
     })})
     $('.Episode6').on("click", function(e){
+        $('#characterList').empty()
+        console.log($('#characterList').children().length)
         e.preventDefault();
-        $('#characterButton').empty()
+        console.log($('#characterList').children().length)
         $('.infoScreen').empty()
-        console.log(Episode6)
         $('.infoScreen').append(`<h1>${Episode6[0].title}`)
         $('.infoScreen').append(`<p>Released: ${Episode6[0].release_date}/ Directed by: ${Episode6[0].director}`)
         $('.infoScreen').append(`<button>Characters`).attr("id","characterButton")
-        $('.mainScreen').text('src',"https://youtu.be/MrDStqPgOEU")
+        $('.mainScreen').attr('src',"https://giphy.com/embed/2SRvACIOCEOys")
         $('#characterButton').on("click", function(e){
-            //e.preventDefault();
+            $('#characterList').empty()
             for (i = 0; i < Episode6[0].characters.length; i++){
-                //console.log(Episode2[0].characters[i])
-                urlSearch(Episode6[0].characters[i])
-                //console.log(name)      
+                urlSearch(Episode6[0].characters[i])      
         }   
     })})
     $('.Episode7').on("click", function(e){
+        $('#characterList').empty()
+        console.log($('#characterList').children().length)
         e.preventDefault();
-        $('#characterButton').empty()
+        console.log($('#characterList').children().length)
         $('.infoScreen').empty()
-        console.log(Episode7)
         $('.infoScreen').append(`<h1>${Episode7[0].title}`)
         $('.infoScreen').append(`<p>Released: ${Episode7[0].release_date}/ Directed by: ${Episode7[0].director}`)
         $('.infoScreen').append(`<button>Characters`).attr("id","characterButton")
-        $('.mainScreen').text('src',"https://youtu.be/MrDStqPgOEU")
+        $('.mainScreen').attr('src',"https://giphy.com/embed/3owzVYjZSzuFivWpHi")
         $('#characterButton').on("click", function(e){
-            //e.preventDefault();
+            $('#characterList').empty()
             for (i = 0; i < Episode7[0].characters.length; i++){
-                //console.log(Episode2[0].characters[i])
-                urlSearch(Episode7[0].characters[i])
-                //console.log(name)      
+                urlSearch(Episode7[0].characters[i])      
         }   
     })})
     
