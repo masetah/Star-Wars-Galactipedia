b90801e6cdd3615d7d88080cd24ef592991308a2
Maysam Taherian <maysamtaherian@Maysams-MacBook-Pro.local>
2019-07-02 12:21:55 -0500

    Better working Demo

--- app2.js
+++ app2.js
@@ -1,22 +1,47 @@
 $(()=>{
 
     $.ajax({
-        url:`https://swapi.co/api/films`
+        url:`https://swapi.co/api/films/`
     }).then(
         (data)=>{
-            console.log(data)
+            //console.log(data)
+            
             Episode1.push(data.results[2]);
             Episode2.push(data.results[1]);
             Episode3.push(data.results[3]);
             Episode4.push(data.results[0]);
             Episode5.push(data.results[5]);
             Episode6.push(data.results[4]);
-            Episode7.push(data.results[6]);  
+            Episode7.push(data.results[6]);
+            console.log(data.results[2].characters)
+            for(i=0;i<data.results[2].characters.length;i++){
+                function pushEp1Names(url){
+                $.ajax({
+                    url: url
+                }).then(
+                    (data)=>{
+                        console.log(data)
+                        //Episode1CharName.push(data)
+                    },
+                    ()=>{
+                        console.log('bad request');
+                    }
+                );
+            }}
+            // Episode1CharactersURL.push(data.results[2].characters);
+            // Episode2CharactersURL.push(data.results[1].characters);
+            // Episode3CharactersURL.push(data.results[3].characters);
+            // Episode4CharactersURL.push(data.results[0].characters);
+            // Episode5CharactersURL.push(data.results[5].characters);
+            // Episode6CharactersURL.push(data.results[4].characters);
+            // Episode7CharactersURL.push(data.results[6].characters);
+             
         },
         ()=>{
             console.log('bad request');
         }
     );
+
     //Global Variables
     const Episode1=[];
     const Episode2=[];
@@ -25,7 +50,34 @@ $(()=>{
     const Episode5=[];
     const Episode6=[];
     const Episode7=[];
-    let charArray=[];
+    const Episode1CharactersURL=[];
+    const Episode2CharactersURL=[];
+    const Episode3CharactersURL=[];
+    const Episode4CharactersURL=[];
+    const Episode5CharactersURL=[];
+    const Episode6CharactersURL=[];
+    const Episode7CharactersURL=[];
+    
+    // console.log(Episode1CharactersURL);
+    // console.log(Episode2CharactersURL);
+    // console.log(Episode3CharactersURL);
+    // console.log(Episode4CharactersURL);
+    // console.log(Episode5CharactersURL);
+    // console.log(Episode6CharactersURL);
+    // console.log(Episode7CharactersURL);
+
+    // for (i = 0; i < Episode1[0].characters.length; i++){
+    //     Episode1CharNames.push(Episode1[0].characters[i])
+    // }
+
+    const Episode1CharNames=[];
+    //console.log(Episode1CharNames);
+    const Episode2CharNames=[];
+    const Episode3CharNames=[];
+    const Episode4CharNames=[];
+    const Episode5CharNames=[];
+    const Episode6CharNames=[];
+    const Episode7CharNames=[];
 
      function urlSearch(url){
         $.ajax({
@@ -33,7 +85,6 @@ $(()=>{
         }).then(
             (data)=>{
                 getCharInfo(data)
-                charArray.push(data)
             },
             ()=>{
                 console.log('bad request');
@@ -44,8 +95,9 @@ $(()=>{
     function getCharInfo(data){
         let name
         name = data.name
-        let $button = $('<button>').attr('class','indivButton')
-        $button.text(name).appendTo($('#characterList'))
+        console.log(data)
+        let $li = $('<button>').attr('id','indivButton') 
+        $li.text(name).appendTo($('#characterList'))
     }
 
     //Episode 1 Button
@@ -57,14 +109,17 @@ $(()=>{
         $('.infoScreen').append(`<p>Released: ${Episode1[0].release_date}/ Directed by: ${Episode1[0].director}`)
         $('.infoScreen').append(`<button>Characters`).attr("id","characterButton")
         $('.mainScreen').attr('src',"https://giphy.com/embed/3owzVXoDN8iP0w3T68")
-        episodeClicked="Episode1"
-        console.log(charArray)
         $('#characterButton').on("click", function(e){
             $('#characterList').empty()
             for (i = 0; i < Episode1[0].characters.length; i++){
                 urlSearch(Episode1[0].characters[i])
             }})
     })
+
+    $('button').click(function(e){
+        window.alert(e.currentTarget.innerHTML);
+    });
+      
 
     //Episode 2 Button
     $('.Episode2').on("click", function(e){
@@ -161,5 +216,8 @@ $(()=>{
                 urlSearch(Episode7[0].characters[i])      
         }   
     })})
+    
+
+
     //For $(()=>{ 
     })
